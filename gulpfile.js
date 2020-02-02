49b282e8ccf40e1f2e79b5e41107bd1d67493bc9
wzn <997402405@qq.com>
2020-02-02 13:52:43 +0800

    lib es 增加css

--- gulpfile.js
+++ gulpfile.js
@@ -10,50 +10,58 @@ const rename = require("gulp-rename");
 const { name, browserList } = require("./package.json");
 const resolve = dir => path.resolve(__dirname, dir);
 const DIR = {
-    scss: resolve("src/components/**/*.scss"),
-    lib: resolve("lib"),
-    es: resolve("es"),
-    dist: resolve("dist")
+  scss: resolve("src/components/**/*.scss"),
+  lib: resolve("lib"),
+  es: resolve("es"),
+  dist: resolve("dist")
 };
 
 const copyScss = () => {
-    return src(DIR.scss)
-        .pipe(dest(DIR.lib))
-        .pipe(dest(DIR.es))
+  return src(DIR.scss)
+    .pipe(dest(DIR.lib))
+    .pipe(dest(DIR.es));
 };
 
 const copyCss = () => {
-    return src(DIR.scss)
-        .pipe(dest(DIR.lib))
-        .pipe(dest(DIR.es))
-}
+  return src(DIR.scss)
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        outputStyle: "compressed"
+      })
+    )
+    .pipe(autoprefixer({ browsers: browserList }))
+    .pipe(size())
+    .pipe(cssnano())
+    .pipe(dest(DIR.lib))
+    .pipe(dest(DIR.es));
+};
 
 const dist = () => {
-    return src(DIR.scss)
-        .pipe(sourcemaps.init())
-        .pipe(
-            sass({
-                outputStyle: "compressed"
-            })
-        )
-        .pipe(autoprefixer({ browsers: browserList }))
-        .pipe(concat(`${name}.css`))
-        .pipe(size())
-        .pipe(dest(DIR.dist))
-        .pipe(sourcemaps.write())
-        .pipe(rename(`${name}.css.map`))
-        .pipe(size())
-        .pipe(dest(DIR.dist))
+  return src(DIR.scss)
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        outputStyle: "compressed"
+      })
+    )
+    .pipe(autoprefixer({ browsers: browserList }))
+    .pipe(concat(`${name}.css`))
+    .pipe(size())
+    .pipe(dest(DIR.dist))
+    .pipe(sourcemaps.write())
+    .pipe(rename(`${name}.css.map`))
+    .pipe(size())
+    .pipe(dest(DIR.dist))
 
-        .pipe(cssnano())
-        .pipe(concat(`${name}.min.css`))
-        .pipe(size())
-        .pipe(dest(DIR.dist))
-        .pipe(sourcemaps.write())
-        .pipe(rename(`${name}.min.css.map`))
-        .pipe(size())
-        .pipe(dest(DIR.dist));
-}
-
+    .pipe(cssnano())
+    .pipe(concat(`${name}.min.css`))
+    .pipe(size())
+    .pipe(dest(DIR.dist))
+    .pipe(sourcemaps.write())
+    .pipe(rename(`${name}.min.css.map`))
+    .pipe(size())
+    .pipe(dest(DIR.dist));
+};
 
 task("default", series(copyScss, copyCss, dist));
